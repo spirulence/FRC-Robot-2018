268843c3cd49a0f377fb7bd18a0555e3a2d3163a
blob324 <dangold324@gmail.com>
2018-01-21 15:23:18 -0800

    created BoxReposition method so the intake can rotate the box whilest holding it.

--- src/org/usfirst/frc/team5700/robot/subsystems/BoxIntake.java
+++ src/org/usfirst/frc/team5700/robot/subsystems/BoxIntake.java
@@ -13,7 +13,8 @@ public class BoxIntake extends Subsystem {
 	private SpeedController leftIntakeMotor = new Spark(RobotMap.LEFT_INTAKE_MOTOR);
 	private SpeedController rightIntakeMotor = new Spark(RobotMap.RIGHT_INTAKE_MOTOR);
 	
-	private double intakeSpeed = 0.8; 
+	private double intakeSpeed = 0.8;
+	private double boxRepositionSpeed = -0.8;
 	
 //	public BoxIntake() {
 //		intakeMotor = new Spark(RobotMap.INTAKE_MOTOR);
@@ -23,6 +24,7 @@ public class BoxIntake extends Subsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+	//This method is for intaking a box.
 	public void setIntakeSpeed(double speed) {
 		leftIntakeMotor.set(speed);
 		rightIntakeMotor.set(speed);
@@ -33,6 +35,17 @@ public class BoxIntake extends Subsystem {
 		leftIntakeMotor.set(intakeSpeed);
 		rightIntakeMotor.set(intakeSpeed);
 		
+	}
+	//This method is for repositioning the box in the intake by making the intake motors spin opposite directions.
+	public void setRepositionSpeed(double posSpeed, double negSpeed){
+		leftIntakeMotor.set(posSpeed);
+		rightIntakeMotor.set(negSpeed);
+		
+	}
+	
+	public void repositionBox() {
+		leftIntakeMotor.set(boxRepositionSpeed);
+		rightIntakeMotor.set(intakeSpeed);
 	}
 
 	public void stopBoxIntake() {
