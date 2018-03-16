5a8efd01f4feae1cdd59b9c323fb79cdd4fa8ebb
Renata Volchinskaya <rvolchinskaya@yahoo.com>
2018-03-15 19:12:12 -0700

    Add button locator class

---
+++ src/org/usfirst/frc/team5700/utils/ButtonLocator.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team5700.utils;
+
+public class ButtonLocator {
+	
+	public final int buttonNum;
+	public final Joystick joystick;
+	
+	public ButtonLocator(int buttonNum, Joystick joystick) {
+		
+		this.buttonNum = buttonNum;
+		this.joystick = joystick;
+		
+	}
+
+}
+
